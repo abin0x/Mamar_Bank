0e21b072a945e3c2aff76453763a1780317035ab
abin0x <mahmudulabin@gmail.com>
2024-07-14 12:54:11 +0600

    Add dj_url hosts

--- mamar_bank/settings.py
+++ mamar_bank/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = 'django-insecure-8nvyn1dfl$vqpg^74h&(3e6^w2!#d^h^3!lgz(-rr&zwgl3wh_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
